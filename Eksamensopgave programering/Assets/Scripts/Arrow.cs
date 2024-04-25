590c318646ff3691c5de6479f82be9e35b1c0dfe
Mihnea <mihn0006@mail.hansenberg.dk>
2024-04-25 13:08:32 +0200

    AI Nav Mesh + Player Shooting + Start Enemy spawn + Kill counter

--- Eksamensopgave programering/Assets/Scripts/Arrow.cs
+++ Eksamensopgave programering/Assets/Scripts/Arrow.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     public Vector2 velocity = new Vector2(0.0f, 0.0f);
+    public GameObject archer;
 
     void Update()
     {
         Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
         Vector2 newPosition = currentPosition + velocity * Time.deltaTime;
 
+        Debug.DrawLine(currentPosition, newPosition, Color.red);
+
         RaycastHit2D[] hits = Physics2D.LinecastAll(currentPosition, newPosition);
 
         foreach (RaycastHit2D hit in hits)
         {
-            Debug.Log(hit.collider.gameObject.name);
+            if (hit.collider.gameObject != archer)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                if (hit.collider.gameObject.tag == "Enemy")
+                {
+                    // Destroy the enemy
+                    Destroy(hit.collider.gameObject);
+                    // Notify the game manager that the enemy died
+                    FindObjectOfType<GameManager>().EnemyDied();
+                }
+                if (hit.collider.gameObject.tag == "Obstacle")
+                {
+                    Destroy(gameObject);
+                }
+            }
         }
-
         transform.position = newPosition;
     }
 }
---
+++ Eksamensopgave programering/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+    private void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
+
