fb1dfd563a77bda1e8dbcbb2d815845b6a198cf1
Mihnea <mihn0006@mail.hansenberg.dk>
2024-04-26 10:33:24 +0200

    small changes

--- Eksamensopgave programering/Assets/Scripts/PlayerStats.cs
+++ Eksamensopgave programering/Assets/Scripts/PlayerStats.cs
@@ -6,23 +6,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerStats : MonoBehaviour
 {
-
     public int health = 100;
     public float movementSpeed = 5f;
     public float shootCooldown;
-
-    void Update()
-    {
-
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            movementSpeed += 1f;
-        }
-
-        if (Keyboard.current.downArrowKey.isPressed)
-        {
-            movementSpeed -= 1f;
-        }
-
-    }
 }
